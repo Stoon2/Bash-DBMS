fe4d46adf4d219dd180ca0bb8918879b8a16c899
Stoon <melsayed.se@gmail.com>
2022-01-01 21:45:00 +0200

    Added splash screen. Added bash headers to all files.

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Later we can edit the $USER to force capitalization on first letter and lower on the rest of username.
+# this will do for now though.
+echo ---------------------------------------------------------------------------
+echo
+echo "                  Welcome ${USER^} to our Bash-DBMS"
+echo "              Made by: Mohamed Elsayed and Mohamed Mohy"
+echo
+echo ---------------------------------------------------------------------------
+

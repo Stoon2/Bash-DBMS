c72b1a8dadb4eff9bf394a8e6306af7755702043
Stoon <melsayed.se@gmail.com>
2022-01-02 21:13:11 +0200

    fixed drop_db

--- db_layer/create_db.sh
+++ db_layer/create_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p "$(pwd)/db_collection/$1/"
 chmod a+x "$(pwd)/db_collection/$1/" # insure execute permission is given on created dirs
+echo $1 is created successfully
--- db_layer/drop_db.sh
+++ db_layer/drop_db.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 PS3="Number:"
-if [ -d db_collection/]
+# if [ -d db_collection/]
 #then
 select s_db in $(ls $(pwd)/db_collection/)
 do
-    read -p "Are you sure you want to delete $s_db ? (Y/N): " pick
-    case $choise in
-        [yY]*) rm -r "$(pwd)/db_collection/$s_db" exit;;
-        [nN]*) echo " Operation Canceled";;
-            *) echo "Invalid option";;
+    read -p "Are you sure you want to delete $s_db ? (Y/N): "
+    case $REPLY in
+        [yY]*) 
+            rm -r "$(pwd)/db_collection/$s_db" 
+            echo $s_db has been dropped successfully
+            exit
+        ;;
+        [nN]*) 
+            echo " Operation Canceled"
+        ;;
+        *) 
+            echo "Invalid option"
+        ;;
     esac
+    exit
 done
 #else
     #echo "$name Database not exist"

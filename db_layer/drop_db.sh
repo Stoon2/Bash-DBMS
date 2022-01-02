83d14bef0fac0528dc5e71920a4ceb38cd8c63d2
mohy.dev <mohy_dev@mohy.dev>
2022-01-02 18:48:58 +0200

    1.1 drop_db.sh

--- db_layer/drop_db.sh
+++ db_layer/drop_db.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+PS3="Number:"
+#if [ -d db_collection/$name]
+#then
 select s_db in $(ls $(pwd)/db_collection/)
 do
-    rm -r "$(pwd)/db_collection/$s_db"
-    exit
+    read -p "Are you sure you want to delete $s_db ? (Y/N): " pick
+    case $choise in
+        [yY]*) rm -r "$(pwd)/db_collection/$s_db" exit;;
+        [nN]*) echo " Operation Canceled";;
+            *) echo "Invalid option";;
+    esac
 done
+#else
+    #echo "$name Database not exist"
+#elseif
+    #echo "Currently there is no Databases"
+#fi

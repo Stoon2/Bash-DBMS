60b890a6f3e5a08c040ba3b21c4d6ddbfde5ba6c
mohy.dev <mohy_dev@mohy.dev>
2022-01-03 05:17:03 +0200

    adjust create

--- db_layer/create_db.sh
+++ db_layer/create_db.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-mkdir -p "$(pwd)/db_collection/$1/"
-chmod a+x "$(pwd)/db_collection/$1/" # insure execute permission is given on created dirs
-echo $1 Database is created successfully
+DIR="db_collection/$1"
+  if [ ! -d "$DIR" ]
+then
+  mkdir -p "$(pwd)/db_collection/$1/"
+  chmod a+x "$(pwd)/db_collection/$1/" # insure execute permission is given on created dirs
+  if [[ $? == 0 ]]
+  then
+    echo "$1 Database is created successfully"
+  else
+    echo "Error Creating Database $1"
+  fi
+else
+    echo "$1 Database is already exist"
+    exit
+fi

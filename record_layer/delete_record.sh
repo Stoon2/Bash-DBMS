989fd89cfde0d3cdea23fb016d886bbc3b0dff52
mohy.dev <mohy_dev@mohy.dev>
2022-01-07 13:27:42 +0200

    delete

--- record_layer/delete_record.sh
+++ record_layer/delete_record.sh
@@ -5,7 +5,8 @@ ht_path="$1/.$2"
 
 curr_delim=$(head -n1 $ht_path | cut -d: -f2) # provides current delimeter from metadata
 escaped_delm=$(echo $curr_delim | sed 's/[^^\\]/[&]/g; s/\^/\\^/g; s/\\/\\\\/g') # delimiter sanitized from regex chars
-# $t_path  > tmp1
+
+cp $t_path tmp1 
 
 if [[ -f $t_path ]]; 
     then
@@ -13,7 +14,6 @@ if [[ -f $t_path ]];
 	read -p "Enter column to delete record from: " colName;
 	read -p "Enter value : " Value;
 	str=\'$Value\'
-	echo $str
 	awk -F"$escaped_delm" -v OFS="$escaped_delm" '
 	{
 		if(NR==1){
@@ -22,25 +22,24 @@ if [[ -f $t_path ]];
 			}
 		}
 		else{
-			if($here=='$Value' || $here==$str ){
+			if($here=='$Value' || $here=="'$str'"){
 				target=NR
 			}
 		}
 		{if(NR!=target)print 
 		}
 	}' $t_path > tmp && mv tmp $t_path;
-	# sed -i ''$target'd' $t_path
-	# echo $target
 else
-	echo "Worng Pick" # reference to table name not table path, leave as is
+	echo " is Worng Pick" # reference to table name not table path, leave as is
 	exit
 fi
 
-# $t_path > tmp2
-# if  cmp -s "$tmp1" "$tmp2"
-# then 
-# echo "The record deleted successfully"
-# else
-# echo "This record cannot be found"
-# fi
+cp $t_path tmp2
+if cmp -s "$tmp1" "$tmp2"
+then
+   echo "Record deleted successfully"
+else
+   echo "Record not found"
+fi
 
+rm tmp1 tmp2
